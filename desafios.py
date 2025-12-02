11359d077310b63775149f464b16701abf41c086
luisaabarros <luisaabarros1707@gmail.com>
2025-12-02 14:58:09 -0300

    adiciona desafio

--- desafios.py
+++ desafios.py
@@ -15,3 +15,59 @@
 #     print('Aluno em recuperação.')
 # else:
 #     print('Aluno em reprovado.')
+
+# DESAFIO 2
+
+# codigo = int(input('Insira o código: '))
+
+# match codigo:
+#     case 1:
+#         print ('Sul')
+#     case 2:
+#         print('Norte')
+#     case 3:
+#         print('Leste')
+#     case 4:
+#         print('Oeste')
+#     case 5 | 6:
+#         print('Nordeste')
+#     case 7 | 8 | 9:
+#         print('Sudeste')
+#     case 10:
+#         print('Centro-oeste')
+#     case 11:
+#         print('Noroeste')
+#     case _:
+#         print('Importado.')
+
+
+
+# DESAFIO 3
+
+valor = float(input('Digite o valor da compra: '))
+av = (valor * 0.10)
+a_vista = valor - av 
+db = (valor * 0.05)
+debito = valor + db
+cd = (valor * 0.08)
+credito = valor + cd
+cq = (valor * 0.12)
+cheque = valor + cq
+
+
+print('Escolha a forma de pagamento: \n[1] À VISTA \n[2] PIX \n[3] DEBITO \n[4] CREDITO \n[5] CHEQUE')
+opcao = int(input('Digite a opção desejada: '))
+
+match opcao:
+    case 1:
+        print(f'Voce escolheu à vista, o valor de {valor} com o desconto ficou {a_vista}')
+    case 2:
+        print(f'Voce escolheu PIX, sua compra ficou {valor}')
+    case 3:
+        print(f'Voce escolheu débito, o valor de {valor} ficou {debito}')
+    case 4:
+        print(f'Voce escolheu crédito, o valor de {valor} ficou {credito}')
+    case 5:
+        print(f'Voce escolheu cheque, o valor de {valor} ficou {cheque}')
+    case _:
+        print('Opção inválida.')
